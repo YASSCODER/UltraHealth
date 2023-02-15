1ddded388bb5634ddb48ec70e1f5be2a02823410
YASSCODER <72893609+YASSCODER@users.noreply.github.com>
2023-02-15 22:26:53 +0100

    fix the version from 6.2 -> 5.4 & create all the entity

--- migrations/Version20230215212110.php
+++ migrations/Version20230215212110.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20230215204926 extends AbstractMigration
+final class Version20230215212110 extends AbstractMigration
 {
     public function getDescription(): string
     {
