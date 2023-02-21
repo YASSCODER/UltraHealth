13101736727e155fc6e7b349fee0c59ae890d8d3
YASSCODER <72893609+YASSCODER@users.noreply.github.com>
2023-02-21 11:00:24 +0100

    create User CRUD + set templates

---
+++ src/Form/AdminType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('cin')
+            ->add('nom')
+            ->add('prenom')
+            ->add('dateNaissance')
+            ->add('zone')
+            ->add('email')
+            ->add('password')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
