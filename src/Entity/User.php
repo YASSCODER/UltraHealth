f22198e1d2858c1fad9d71667edf0432d592c64c
YASSCODER <72893609+YASSCODER@users.noreply.github.com>
2023-03-02 15:32:54 +0100

    create security & auth functions to make registration with session and unique profile

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -7,11 +7,14 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 #[ORM\Table(name: '`user`')]
-class User
+#[UniqueEntity(fields: ['email'], message: 'There is already an account with this email')]
+class User implements UserInterface
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
@@ -104,7 +107,15 @@ class User
 
         return $this;
     }
-
+/**
+         * A visual identifier that represents this user.
+         *
+         * @see UserInterface
+         */
+        public function getUsername(): string
+        {
+                return (string) $this->username;
+        }
     public function getNom(): ?string
     {
         return $this->nom;
@@ -121,6 +132,7 @@ class User
     {
         return $this->prenom;
     }
+    
 
     public function setPrenom(string $prenom): self
     {
@@ -153,30 +165,59 @@ class User
         return $this;
     }
 
-    public function getEmail(): ?string
+    public function getemail(): ?string
     {
         return $this->email;
     }
 
-    public function setEmail(string $email): self
+    public function setemail(string $email): self
     {
         $this->email = $email;
 
         return $this;
     }
 
-    public function getPassword(): ?string
+    /**
+         * @see UserInterface
+         */
+    public function getpassword(): ?string
     {
         return $this->password;
     }
 
-    public function setPassword(string $password): self
+    public function setpassword(string $password): self
     {
         $this->password = $password;
 
         return $this;
     }
 
+    /**
+         * @see UserInterface
+         */
+        public function getRoles(): array
+        {
+                $roles = $this->roles;
+                // guarantee every user at least has ROLE_USER
+                $roles[] = 'ROLE_USER';
+
+                return array_unique($roles);
+        }
+     /**
+         * @see UserInterface
+         */
+        public function eraseCredentials()
+        {
+                // If you store any temporary, sensitive data on the user, clear it here
+                // $this->plainPassword = null;
+        }
+    /**
+         * @see UserInterface
+         */
+        public function getSalt()
+        {
+                // not needed when using the "bcrypt" algorithm in security.yaml
+        }
     public function getRole(): ?string
     {
         return $this->role;
